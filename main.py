6e9760cfd4134f407ef41c91b210bad87898776a
Savaliya-Anvr <ravi.s@anvtech.co>
2024-01-17 18:14:16 +0530

    FastAPI

--- main.py
+++ main.py
@@ -2,28 +2,33 @@ from typing import Union
 import uvicorn
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/blog/unpublished')
-def unpublished():
-    return {'Data':'This is unpublished Data'}
+class Blog(BaseModel):
+    title:str
+    body:str
+    pub: Optional[bool] = True
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    return {'data': f"{blog.title} is title of the blog"}
 
 @app.get("/blog")
-def read_root(item = 10, published:bool = True , sort:Optional[str] = 'ok'):
+def read_root(item=10, published: bool = True, sort: Optional[str] = 'ok'):
     if published:
         return {"Hello": f"World {item} and {sort}"}
     else:
-        return {'ok':f'{item} is not published yet'}
-    return {'ok': f'{item} is not published yet but will soon do it'}
+        return {'ok': f'{item} is not published yet'}
+    # return {'ok': f'{item} is not published yet but will soon do it'}
 
 
+@app.get('/blog/{ids}')
+def read_item(ids: int):
+    return {"item_id": ids}
 
-@app.get('/blog/{id}')
-def read_item(id:int):
-    return {"item_id": id}
 
 @app.get('/blog/{id}/comment')
-def read_item(id:int,limit = 78):
+def read_item(id: int, limit=78):
     return limit
-    return {"item_id": {'1st cmnt','2nd cmnt'}}
